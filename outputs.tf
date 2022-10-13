fe3bd1ab1a142e54856b115ef84f797921703a76
scotttyso <tyscott@cisco.com>
2022-10-13 00:08:31 -0400

    updating key names

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,28 @@
 output "access" {
-  description = "Admin module outputs."
+  description = "Access module outputs."
   value       = module.access
 }
 
-#output "access" {
-#  description = "Admin module outputs."
-#  value       = local.model.access.policies.interface
-#}
-#
+output "admin" {
+  description = "Admin module outputs."
+  value       = module.admin
+}
+
+output "fabric" {
+  description = "Fabric module outputs."
+  value       = module.fabric
+}
+
+output "switch" {
+  description = "Switch module outputs."
+  value       = module.switch
+}
+
+output "system_settings" {
+  description = "System Settings module outputs."
+  value       = module.system_settings
+}
+
 #output "defaults_access" {
 #  description = "Admin module outputs."
 #  value       = local.model.defaults.access.pre_built_interface_policies
